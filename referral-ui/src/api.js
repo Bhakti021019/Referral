e2bfdb4352e64681c76ba20786ea8325530b193a
Bhakti Modak <modakbhakti15@gmail.com>
2025-09-08 15:39:15 +0530

    Updated pages with latest changes

--- referral-ui/src/api.js
+++ referral-ui/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+});
 
 // If logged in, attach JWT token
 API.interceptors.request.use((req) => {
